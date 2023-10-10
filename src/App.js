5a58503660285b3daa4a27782a683daae00d7e8d
Xinyi Ai <xinyiai@wirelessprv-10-194-37-207.near.illinois.edu>
2023-10-10 17:19:09 -0500

    add componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      hello world!
+      <div className='search-bar-container'>
+        <div>search bar</div>
+        
+      </div>
+      <div className='switch'>
+        <div>Route
+          <div>list</div>
+          <div>GalleryView</div>
+        </div>
+        
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +29,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
